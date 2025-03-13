f45f881e6d2a569c263e4bf473eb7b5340695280
rizkibagus <brizki121@gmail.com>
2025-03-13 10:36:39 +0700

    feat: add function search button for this project

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ let page = 1; //ini untuk pagination
 const API_URL = () =>
   `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&page=${page}`;
 const API_IMAGE_URL = "https://image.tmdb.org/t/p/w1280";
+const API_SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
 
 async function getMovies(url) {
   const res = await fetch(url);
@@ -12,17 +13,22 @@ async function getMovies(url) {
   showMovies(data.results);
 }
 
+function updatePage() {
+  getMovies(API_URL());
+  currentPage.innerHTML = page;
+}
+
 function nextPage() {
   if (page >= 1) {
     page += 1;
-    getMovies(API_URL());
+    updatePage();
   }
 }
 
 function prevPage() {
   if (page > 1) {
     page -= 1;
-    getMovies(API_URL());
+    updatePage();
   }
 }
 
@@ -54,4 +60,18 @@ function showMovies(movies) {
   });
 }
 
-getMovies(API_URL());
+searchForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const searchQuery = search.value;
+
+  if (searchQuery !== "") {
+    getMovies(API_SEARCH_URL + searchQuery);
+    search.value = "";
+  }
+});
+
+updatePage();
+
+title.addEventListener("click", () => {
+  location.reload();
+});
